00fec1682bdc9b637a4cefbdbd2cb2148352d9bd
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-06-15 15:35:03 +0700

    Update ReactNativeProgressPage.tsx

--- pages/ReactNativeProgressPage.tsx
+++ pages/ReactNativeProgressPage.tsx
@@ -5,10 +5,10 @@ import * as Progress from "react-native-progress";
 const ReactNativeProgressPage = () => {
   return (
     <View style={styles.container}>
-      {/* <Progress.Circle size={30} indeterminate={true} /> */}
-      {/* <Progress.Bar progress={0.9} width={200} /> */}
-      {/* <Progress.Pie progress={0.4} size={50} /> */}
-      <Progress.CircleSnail color={['red', 'green', 'blue']} />
+      <Progress.Circle size={30} indeterminate={true} />
+      <Progress.Bar progress={0.9} width={200} />
+      <Progress.Pie progress={0.4} size={50} />
+      <Progress.CircleSnail color={["red", "green", "blue"]} />
     </View>
   );
 };
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    rowGap: 20,
     alignItems: "center",
   },
 });
